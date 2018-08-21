e161a59fc10f8f6b6b2c47fa07744d4e492993d2
mosy <479979298@qq.com>
2018-08-21 17:54:14 +0800

    修改pom依赖

---
+++ src/main/java/com/ssm/core/configuration/LiquibaseConfig.java
@@ -0,0 +1,17 @@
+package com.ssm.core.configuration;
+
+import javax.sql.DataSource;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import liquibase.integration.spring.SpringLiquibase;
+
+//@Configuration
+//public class LiquibaseConfig {
+//	@Bean
+//   public SpringLiquibase liQuiBase(DataSource dataSource) {
+//	return null;
+//	   
+//   }
+//}
