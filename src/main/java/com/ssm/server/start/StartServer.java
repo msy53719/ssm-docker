3cf14d38c6520d35514425c1e0e3636f639d5035
mosy <479979298@qq.com>
2018-08-22 17:53:49 +0800

    add liquibase bean

--- src/main/java/com/ssm/server/start/StartServer.java
+++ src/main/java/com/ssm/server/start/StartServer.java
@@ -2,6 +2,8 @@ package com.ssm.server.start;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.liquibase.LiquibaseProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.ComponentScan;
 
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -9,6 +11,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @SpringBootApplication
 @ComponentScan(basePackages = {"com.ssm.core"}) 
 @EnableSwagger2
+@EnableConfigurationProperties({LiquibaseProperties.class })  
 public class StartServer {
 
 	public static void main(String[] args) {
