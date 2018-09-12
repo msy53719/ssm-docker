c8498647ca580908f5aee00fd5e38186ea583347
mosy <479979298@qq.com>
2018-09-12 22:28:32 +0800

    add liqubase

--- src/main/java/com/ssm/core/configuration/LiquibaseConfig.java
+++ src/main/java/com/ssm/core/configuration/LiquibaseConfig.java
@@ -1,29 +1,28 @@
-//package com.ssm.core.configuration;
-//
-//import javax.sql.DataSource;
-//
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
-//import org.springframework.boot.autoconfigure.liquibase.LiquibaseProperties;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import liquibase.integration.spring.SpringLiquibase;
-//
-//@Configuration
-//public class LiquibaseConfig {
-//	@Autowired
-//	DataSourceConfig dataSource;
-//
-//	@Bean
-//	public SpringLiquibase liquibase(DataSource dataSource, DataSourceProperties dataSourceProperties,
-//			LiquibaseProperties liquibaseProperties) {
-//		SpringLiquibase liquibase = new SpringLiquibase();
-//		liquibase.setDataSource(dataSource);
-//		liquibase.setChangeLog(liquibaseProperties.getChangeLog());
-//		liquibase.setContexts(liquibaseProperties.getContexts());
-//		liquibase.setDefaultSchema(liquibaseProperties.getDefaultSchema());
-//		liquibase.setDropFirst(liquibaseProperties.isDropFirst());
-//		liquibase.setShouldRun(liquibaseProperties.isEnabled());
-//		return liquibase;
-//	}
-//}
+package com.ssm.core.configuration;
+
+import javax.sql.DataSource;
+
+import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
+import org.springframework.boot.autoconfigure.liquibase.LiquibaseProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import liquibase.integration.spring.SpringLiquibase;
+
+@Configuration
+@EnableConfigurationProperties({ LiquibaseProperties.class })
+public class LiquibaseConfig {
+
+	@Bean
+	public SpringLiquibase liquibase(DataSource dataSource, DataSourceProperties dataSourceProperties,
+			LiquibaseProperties liquibaseProperties) {
+		SpringLiquibase liquibase = new SpringLiquibase();
+		liquibase.setDataSource(dataSource);
+		liquibase.setChangeLog(liquibaseProperties.getChangeLog());
+		liquibase.setContexts(liquibaseProperties.getContexts());
+		liquibase.setDefaultSchema(liquibaseProperties.getDefaultSchema());
+		liquibase.setDropFirst(liquibaseProperties.isDropFirst());
+		liquibase.setShouldRun(liquibaseProperties.isEnabled());
+		return liquibase;
+	}
+}
--- src/main/java/com/ssm/server/start/StartServer.java
+++ src/main/java/com/ssm/server/start/StartServer.java
@@ -2,8 +2,6 @@ package com.ssm.server.start;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.liquibase.LiquibaseProperties;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.ComponentScan;
 
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -11,7 +9,6 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @SpringBootApplication
 @ComponentScan(basePackages = { "com.ssm.core" })
 @EnableSwagger2
-@EnableConfigurationProperties({ LiquibaseProperties.class })
 public class StartServer {
 
 	public static void main(String[] args) {
