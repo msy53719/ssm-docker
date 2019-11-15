a23020f0a2f8b9c16b927d10e8701f48ef9aeee2
mosy <479979298@qq.com>
2019-11-15 17:22:53 +0800

    add

--- src/main/java/com/ssm/core/controller/HelloController.java
+++ src/main/java/com/ssm/core/controller/HelloController.java
@@ -4,9 +4,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping(value = "/")
 public class HelloController {
 
-	@RequestMapping(value = "/")
+	@RequestMapping(value = "index")
 	public String index() {
 		return "Greetings from Spring Boot!";
 	}
